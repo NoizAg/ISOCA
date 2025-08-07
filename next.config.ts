2628787e6e664e5d950fae78aa42ae0f8396230e
NoizAg <sicoex@gmail.com>
2025-08-07 20:24:45 -0300

    optimización

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: false
   },
   /* config options here */
 };
