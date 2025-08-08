380daafae33da1515a14a628eda49de435db22d0
NoizAg <sicoex@gmail.com>
2025-08-07 21:56:52 -0300

    Fix: Disable CSS optimization temporarily and update README

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,9 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  experimental: {
-    optimizeCss: true,
-  },
+  // experimental: {
+  //   optimizeCss: true,
+  // },
   compress: true,
   poweredByHeader: false,
   /* config options here */
